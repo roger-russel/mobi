f17d13393bd6d4976292f0f6a3205d7a4f0191d0
Roger Russel <roger.russel@dafiti.com.br>
2020-09-05 17:33:47 -0300

    debug option

--- opt.go
+++ opt.go
@@ -4,10 +4,5 @@ var options Options
 
 //Options Struct
 type Options struct {
-	debug bool
-}
-
-//IsDebug return bool if is debug
-func (o Options) IsDebug() bool {
-	return o.debug
+	Debug bool
 }
